3a5cc08873720420ef8297800966e88f10dc433b
aishwarya42 <42571918+aishwarya42@users.noreply.github.com>
2023-07-25 23:37:21 +0530

    Create generate_binary_string.java

---
+++ assignment/assignment-6(recursion_backtracking)/generate_binary_string.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        String ans = "";
+        while(t>0){
+            String s = sc.next();
+            wild_card(s,ans);
+            t--;
+        }
+    }
+
+  public static void wild_card(String ques, String ans){
+        if(ques.length()==0){
+            System.out.print(ans+" ");
+            return;
+        }
+        char ch = ques.charAt(0);
+            if(ch=='?'){
+                wild_card(ques.substring(1),ans+0);
+                wild_card(ques.substring(1),ans+1);
+            }
+            else{
+                wild_card(ques.substring(1),ans+ch);
+            }
+
+ 
+    }
+}
